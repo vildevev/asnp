5fce5ca38a5e59bf102244d8056ddeb947d7f307
Vilde Vevatne <vildevevatne@Vildes-MacBook-Pro.local>
2023-04-09 20:28:36 -0700

    feat: adds react fe

---
+++ asnm-fe/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const displayQuestions = false;
+  const questions = "\n\n1. What other hobbies do you have?\n2. What do you like to do in your free time?\n3. What do you think about the current state of the world?\n4. What do you think are the biggest challenges facing society today?\n5. What do"
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Awkward Silence No More</h1>
+        {
+          displayQuestions && (
+            <div dangerouslySetInnerHTML={{__html: questions}} />
+          )
+        }
+      </header>
+    </div>
+  );
+}
+
+export default App;
