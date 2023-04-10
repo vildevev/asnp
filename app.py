9ccf7cdf46eda935b5a2543a915ebf56df1c3259
Vilde Vevatne <vildevevatne@Vildes-MacBook-Pro.local>
2023-04-09 19:53:45 -0700

    feat: updates conversation prompt and parses response

--- app.py
+++ app.py
@@ -12,8 +12,22 @@ openai.api_key = os.environ.get("OPEN_API_KEY")
 @app.route("/")
 def home():
     f = open('recording.txt','r')
-    prompt = f'Suggest several questions to ask each other next based on the conversation between these people: {f}'
+    prompt = f'Suggest multiple things to talk about next based on this conversation: {f}'
     response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0, max_tokens=7)
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=60,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    text = response["text"]
+    file = open('recording_response.txt', 'w')
+    file.write(transcript["text"])
+    file.close()
+
     return response
 
 @app.route("/record")
@@ -24,3 +38,10 @@ def record():
     file.write(transcript["text"])
     file.close()
     return transcript
+
+@app.route("/format")
+def format():
+    # TODO: read from open API response
+    # TODO: do this realtime
+    response = "\n\n1. What other hobbies do you have?\n2. What do you like to do in your free time?\n3. What do you think about the current state of the world?\n4. What do you think are the biggest challenges facing society today?\n5. What do"
+    return response
